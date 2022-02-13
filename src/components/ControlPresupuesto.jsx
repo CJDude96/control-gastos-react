be0e042cb0b7200b98d02cc7c609d3a745dadbed
CJDude96 <cjsanche96@gmail.com>
2022-02-13 03:34:16 -0400

    Practica finalizada

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const ControlPresupuesto = ({
+  presupuesto,
+  setPresupuesto,
+  gastos,
+  setGastos,
+  setIsValidPresupuesto,
+}) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+  const [porcentaje, setPorcentaje] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce((total, gasto) => {
+      return gasto.cantidad + total;
+    }, 0);
+
+    const totalDisponible = presupuesto - totalGastado;
+
+    const nuevoPorcentaje = (
+      ((presupuesto - totalDisponible) / presupuesto) *
+      100
+    ).toFixed(2);
+
+    setPorcentaje(nuevoPorcentaje);
+    setDisponible(totalDisponible);
+    setGastado(totalGastado);
+  }, [gastos]);
+
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  const handleResetApp = () => {
+    const resultado = confirm("¿Desea reiniciar la app?");
+    if (resultado) {
+      setGastos([]);
+      setPresupuesto(0);
+      setIsValidPresupuesto(false);
+    }
+  };
+
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div>
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: porcentaje > 100 ? "#dc2626" : "#3b82f6",
+            trailColor: "#d1d1d1",
+            textColor: porcentaje > 100 ? "#dc2626" : "#3b82f6",
+          })}
+          value={porcentaje}
+          text={`Gastado: ${porcentaje}%`}
+        />
+      </div>
+      <div className="contenido-presupuesto">
+        <button className="reset-app" type="button" onClick={handleResetApp}>
+          Reiniciar App
+        </button>
+        <p>
+          <span>Presupuesto: </span>
+          {formatearCantidad(presupuesto)}
+        </p>
+        <p className={`${disponible < 0 ? "negativo" : ""}`}>
+          <span>Disponible: </span>
+          {formatearCantidad(disponible)}
+        </p>
+        <p>
+          <span>Gastado: </span>
+          {formatearCantidad(gastado)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ControlPresupuesto;
